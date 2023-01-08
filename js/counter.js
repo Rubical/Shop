407caa73bc9640077ec528c9b27c76261914c9d0
Rubical <zhpiv96@mail.ru>
2023-01-08 18:42:29 +0300

    added counter functional

---
+++ js/counter.js
@@ -0,0 +1,17 @@
+const wrapper = document.querySelector(".row");
+
+wrapper.addEventListener("click", function (event) {
+  if (event.target.dataset.action == "minus") {
+    const parentNode = event.target.closest(".counter-wrapper");
+    let counter = parentNode.querySelector("div[data-counter]");
+    Number(counter.textContent) !== 1
+      ? (counter.textContent = --counter.textContent)
+      : null;
+  }
+
+  if (event.target.dataset.action == "plus") {
+    const parentNode = event.target.closest(".counter-wrapper");
+    let counter = parentNode.querySelector("div[data-counter]");
+    counter.textContent = ++counter.textContent;
+  }
+});
